69cf0da13235dbd68f70ee884dcc824cb2f2ad18
lilibeigithub <lili.bei@outlook.com>
2018-10-19 12:43:23 +0200

    Done the exercise

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,48 @@
 <?php
 
 $input;
-$winner;
+$A = 0;
+$B = 0;
+
+foreach ($input as $value) {
+    if($value[0] > $value[1])
+        {
+            $A++ ;
+        }
+        else if($value[0] < $value[1])
+        {
+            $B++ ;
+        }
+    }
+echo ($A);
+echo ($B);
+
+if($A > $B)
+{
+    $winner = 'A';
+}
+else
+{
+    $winner = 'B';
+}
+
+/* 
+foreach ($a as $v1) {
+    foreach ($v1 as $v2) {
+        echo "$v2\n";
+    }
+}
+
+foreach($v1 as $v2)
+    {
+        if($v1 > $v2)
+        {
+            ++brotherA;
+        }
+        else if($v2 > $v1)
+        {
+            ++brotherB;
+        }
+
+
+*/
